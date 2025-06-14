9743beab276cc44f8650d416a59df29373de9888
https://learn.co/arf2k <foreman.alexander@gmail.com>
2025-06-14 18:21:18 -0400

    update dexie db for stats persistence and update word list

--- src/db.js
+++ src/db.js
@@ -1,16 +1,13 @@
-// src/db.js
 import Dexie from 'dexie';
 
 export const db = new Dexie('flashcardAppDB');
 
-// Version 1: Your original schema
 db.version(1).stores({
   appState: 'id',
   hardWords: '[spanish+english]',
   allWords: '++id, spanish, english'
 });
 
-// Version 2: Added 'category' index to allWords
 db.version(2).stores({
   appState: 'id', 
   hardWords: '[spanish+english]', 
@@ -20,7 +17,6 @@ db.version(2).stores({
   return tx.table('allWords').count();
 });
 
-// Version 3: Added SRS/Leitner data fields to the word objects
 db.version(3).stores({
   allWords: '++id, spanish, english, category, dueDate'
 }).upgrade(async (tx) => {
@@ -33,7 +29,6 @@ db.version(3).stores({
     });
 });
 
-// Version 4: Added leitnerBox index for querying
 db.version(4).stores({
     appState: 'id',
     hardWords: '[spanish+english]',
@@ -43,25 +38,28 @@ db.version(4).stores({
     return Promise.resolve();
 });
 
-// --- NEW: Version 5 to migrate all words to the "Box 0" / Unseen Pool ---
+
 db.version(5).stores({
-    // The schema string for the stores does not need to change from version 4
     appState: 'id',
     hardWords: '[spanish+english]',
     allWords: '++id, spanish, english, category, dueDate, leitnerBox'
 }).upgrade(async (tx) => {
-    // This upgrade function will run once for every user.
-    // Its job is to move all existing cards into the "unseen" pool (Box 0).
-    console.log("Attempting to upgrade database to version 5 for Leitner Box 0 system...");
+
     
     await tx.table('allWords').toCollection().modify(word => {
-        // Move every card to Box 0 and nullify its due date.
+     
         word.leitnerBox = 0;
         word.dueDate = null;
-        // We can leave lastReviewed as is, or nullify it too. Let's nullify for consistency.
+
         word.lastReviewed = null;
     });
 
     const count = await tx.table('allWords').count();
     console.log(`Database upgrade to version 5 successful. All ${count} existing words moved to Leitner Box 0.`);
+});
+db.version(6).stores({
+    appState: 'id',
+    hardWords: '[spanish+english]',
+    allWords: '++id, spanish, english, category, dueDate, leitnerBox',
+    dailyStats: 'date' 
 });
